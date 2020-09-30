7331e75d95d67492746b32d2584b0a953634bf27
j1v3 <jeremy.vincent60@gmail.com>
2020-09-30 14:25:02 +0200

    Chapter6

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,17 +3,31 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Componetn\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/conference", name="conference")
+     * @Route("/{name}", name="homepage")
      */
-    public function index()
+    public function index(string $name = "")
     {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);
+        $greet = "";
+        if ($name) {
+            $greet = \sprintf("Et hop %s !!!", \htmlspecialchars($name));
+        }
+
+        return new Response(
+            <<<EOF
+            <html>
+                <body>
+                $greet
+                    <img src="images/under-construction.gif">
+                </body>
+            </html>
+            EOF
+        );
     }
 }
